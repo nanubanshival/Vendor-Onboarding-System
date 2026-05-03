f825073fd254b9136662a8ac3d6a079438638785
Nanu Banshival <nanubanshival@gmail.com>
2026-05-03 21:07:26 +0530

    Added project setup and static config files
    Added project setup (sln, appsetting, program.cs, csproj) , wwwroot

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using VendorOnboardingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendorOnboardingSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddSession();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Admin}/{action=Login}/{id?}");
+
+            app.Run();
+        }
+    }
+}
